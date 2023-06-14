3110125f518a2839bc34d1e10ec238225ca96525
Rikerdaves <rikerdaves.alencar@gmail.com>
2023-06-14 04:42:00 -0300

    Web filmes

--- web_filmes/src/Components/Cardmovie/index.js
+++ web_filmes/src/Components/Cardmovie/index.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import '../Cardmovie/style.css'
+import '../Cardmovie/style.css';
 
 const MovieList = () => { 
   const [movies, setMovies] = useState([]);
@@ -9,20 +9,18 @@ const MovieList = () => {
 
   const navigate = useNavigate();
 
-
   const apiKey = '8a78bbc2059ae1af9b5db720e9ee991d';
   
   const getMovies = async (page) => {
     const response = await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=${page}`);
     return response.data.results;
-  }
+  };
 
   const loadMovies = async () => {
     const newMovies = await getMovies(currentPage);
     setMovies([...movies, ...newMovies]);
-  }
+  };
 
-  // eslint-disable-next-line
   const handleScroll = () => {
     const scrollTop = (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
     const scrollHeight = (document.documentElement && document.documentElement.scrollHeight) || document.body.scrollHeight;
@@ -31,36 +29,38 @@ const MovieList = () => {
     if (scrolledToBottom) {
       setCurrentPage(currentPage + 1);
     }
-  }
+  };
 
   useEffect(() => {
     window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
-  }, [handleScroll]);
-  
+    return () => window.removeEventListener('scroll', handleScroll);// eslint-disable-next-line 
+  }, []); 
 
   useEffect(() => {
-    loadMovies();// eslint-disable-next-line
+    loadMovies(); // eslint-disable-next-line 
   }, [currentPage]);
 
   const handleMovieClick = (id) => {
-    // Redirect to the movie details page
     navigate(`/filme/${id}`);
-  }
+  };
 
   return (
-    <div className='content'>
-      <h2>Filmes Populares</h2>
-      <ul className="lista-colunas">
+      <>
+      <h2 className="titulo">Filmes Populares</h2>
+      <div className='content'>
+      <ul className="lista-colunas-5">
         {movies.map((movie) => (
-          <>
-          <li key={movie.id} ></li>
-          <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} 
-          alt={`Poster do filme ${movie.title}`} onClick={() => handleMovieClick(movie.id)}/>
-          </>
+          <li key={movie.id}>
+            <img
+              src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+              alt={`Poster do filme ${movie.title}`}
+              onClick={() => handleMovieClick(movie.id)}
+            />
+          </li>
         ))}
       </ul>
     </div>
+    </>
   );
 };
 
